c845af04534dcfb940f9fd744b34b83ea2a9338c
BrendaMwiza <brendabrizy@gmail.com>
2019-11-21 11:12:56 +0200

    changed styles

--- static/js/custom.js
+++ static/js/custom.js
@@ -120,19 +120,19 @@ jQuery(document).ready(function($) {
 	});
 
 	// flexslider main
-	$('#main-flexslider').flexslider({						
-		animation: "swing",
-		direction: "vertical", 
-		slideshow: true,
-		slideshowSpeed: 3500,
-		animationDuration: 1000,
-		directionNav: true,
-		prevText: '<i class="icon-angle-up icon-2x"></i>',       
-		nextText: '<i class="icon-angle-down icon-2x active"></i>', 
-		controlNav: false,
-		smootheHeight:true,						
-		useCSS: false
-	});
+	// $('#main-flexslider').flexslider({						
+	// 	animation: "swing",
+	// 	direction: "vertical", 
+	// 	slideshow: true,
+	// 	slideshowSpeed: 3500,
+	// 	animationDuration: 1000,
+	// 	directionNav: true,
+	// 	prevText: '<i class="icon-angle-up icon-2x"></i>',       
+	// 	nextText: '<i class="icon-angle-down icon-2x active"></i>', 
+	// 	controlNav: false,
+	// 	smootheHeight:true,						
+	// 	useCSS: false
+	// });
 });
 	
 	
